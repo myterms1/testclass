70d2038a840e276ab1e6a9fd5642f70c84192209
myterms1 <104558105+myterms1@users.noreply.github.com>
2024-12-12 00:48:08 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,24 +1,14 @@
-output "arn" {
+output "lambda_arn" {
   description = "ARN of the Lambda function"
   value       = aws_lambda_function.lambda.arn
 }
 
-output "invoke_arn" {
-  description = "ARN to invoke the Lambda function via API Gateway"
-  value       = aws_lambda_function.lambda.invoke_arn
-}
-
-output "function_name" {
+output "lambda_name" {
   description = "Name of the Lambda function"
   value       = aws_lambda_function.lambda.function_name
 }
 
-output "version" {
-  description = "Latest published version of the Lambda function"
-  value       = aws_lambda_function.lambda.version
-}
-
-output "log_group" {
-  description = "Log group created for the Lambda function"
-  value       = aws_cloudwatch_log_group.log_group.name
+output "execution_role_arn" {
+  description = "ARN of the Lambda execution role"
+  value       = aws_iam_role.lambda_exec_role.arn
 }
