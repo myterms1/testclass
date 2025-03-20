a6070f175ae8eefd9aa616149ecd4c1988899cb8
myterms1 <104558105+myterms1@users.noreply.github.com>
2025-03-20 01:19:14 -0400

    Update thisone.tf

--- thisone.tf
+++ thisone.tf
@@ -1,3 +1,15 @@
 vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
 node_subnet_ids = flatten([for s in data.terraform_remote_state.vpc.outputs.additional_subnets : s.id])
+
+
+data "aws_subnets" "pod_subnets" {
+  filter {
+    name   = "tag:Name"
+    values = ["broker-apps-dev-non-routable-golden-subnet-*"]
+  }
+}
+
+locals {
+  pod_subnet_ids = data.aws_subnets.pod_subnets.ids
+}
